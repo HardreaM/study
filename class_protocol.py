a94e7b54076e0332bbb9372458c46d8caf2f2bcb
HardreaM <46752979+HardreaM@users.noreply.github.com>
2021-12-07 17:18:54 +0500

    Add files via upload

--- class_protocol.py
+++ class_protocol.py
@@ -1,12 +1,32 @@
 class Value:
 
-    def __init__ (self, tarif1, tarif2, period, room):
-        self.__tarif1 = tarif1
-        self.__tarif2 = tarif2
-        self.__period = period
-        self.__room = room
+    def __init__ (self, data):
+        self.__tarif1, self.__tarif2, self.__period, self.__room = data
 
-    def encode (data, bits):
+    def get_all (self):
+        return self.__tarif1, self.__tarif2, self.__period, self.__room
+
+    def encode (self):
+
+        tarif1 = Value.formate(int(self.__tarif1), 3)
+        tarif2 = Value.formate(int(self.__tarif2), 3)
+        period = Value.formate(int(self.__period), 1)
+        room = Value.formate(int(self.__room), 2)
+
+        out = tarif1 + tarif2 + period + room
+
+        return out
+
+    def decode (data):
+
+        tarif1 = Value.deformate(data[0:3], 3)
+        tarif2 = Value.deformate(data[3:6], 3)
+        period = Value.deformate(data[6], 1)
+        room = Value.deformate(data[7::], 2)
+
+        return tarif1, tarif2, period, room
+
+    def formate (data, bits):
 
         out = []
 
@@ -22,13 +42,24 @@ class Value:
         
         return out
 
-    def send (self):
+    def deformate (data, bits):
 
-        tarif1 = self.encode(int(self.tarif1), 3)
-        tarif2 = self.encode(int(self.tarif2), 3)
-        period = self.encode(int(self.period), 1)
-        room = self.encode(int(self.room), 2)
+        if bits == 1:
+            return data
 
-        out = tarif1 + tarif2 + period + room
+        out = 0
+        c = 0
+
+        for i in range(bits-1, -1, -1):
+
+            out = out | (data[c]<<(i*8))
+            c += 1
 
         return out
+
+
+
+data = '851 31 8 1943'
+value1 = Value(list(map(int, data.split())))
+print(value1.get_all())
+print(Value.decode(value1.encode()))
